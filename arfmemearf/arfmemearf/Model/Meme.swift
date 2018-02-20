f1272c1d4a311d3910a6a1d70b9568aa9286e345
Kaira Villanueva <kaira@Kairas-Air.home>
2018-02-19 22:40:17 -0500

    repo

---
+++ arfmemearf/arfmemearf/Model/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MakeMeme
+//
+//  Created by Kaira Villanueva on 2/5/18.
+//  Copyright © 2018 Kaira Villanueva. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Meme {
+    
+    var memeImage: UIImage?
+    var memeText: String?
+    
+}
